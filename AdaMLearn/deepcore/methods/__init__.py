76b00c01f5c5978fe8a1149b56cb6ca3060cc074
xhgb <75128995+xhgb@users.noreply.github.com>
2026-01-04 15:34:44 +0800

    Delete AdaMLearn directory

--- AdaMLearn/deepcore/methods/__init__.py
+++
@@ -1,8 +0,0 @@
-from .contextualdiversity import *
-from .coresetmethod import *
-from .earlytrain import *
-from .full import *
-from .kcentergreedy import *
-from .submodular import *
-from .uniform import *
-
